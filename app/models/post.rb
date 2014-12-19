9e8f4c21c643cffd0323a059b5e8e6a88d3b389e
Domas <domas.bitvinskas@me.com>
2014-12-19 16:44:50 +0200

    Make related post dependent

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   serialize :data
-  has_one :related_post
+  has_one :related_post, dependent: :destroy
 
   def method_missing(name, *args)
     super rescue "#{portal}Post".classify.constantize.new(self).send(name)
